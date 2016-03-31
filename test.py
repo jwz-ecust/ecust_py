4adbdf33106e5194cfea7ab6fd3bb5841d720d9e
zjw <zhangjiawei@zjwdeMacBook-Pro.local>
2016-04-01 01:31:12 +0800

    Update 2016-04-01T01:31:12.905624

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ start = now.replace(year=2016)
 
 
 def change_day():
-return datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
+    return datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
 
 
 def change_time():
-return datetime.timedelta(seconds=randint(0, 60), minutes=randint(0, 60))
+    return datetime.timedelta(seconds=randint(0, 60), minutes=randint(0, 60))
 
 
 commit_date = (start + change_day())
@@ -19,11 +19,11 @@ times = randint(5, 30)
 
 while commit_date < now:
 
-commit_date = commit_date + change_day()
-for i in range(times):
-f = open('data.txt', 'a+')
-commit_date = commit_date + change_time()
-f.writelines(commit_date.isoformat() + '\n')
-f.close()
-os.system('git add .')
-os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat())) 
+    commit_date = commit_date + change_day()
+    for i in range(times):
+        f = open('data.txt', 'a+')
+        commit_date = commit_date + change_time()
+        f.writelines(commit_date.isoformat() + '\n')
+        f.close()
+        os.system('git add .')
+        os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
