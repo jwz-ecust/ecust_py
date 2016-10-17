f24b618e493e7db6b0387e0c64decf7640de2fea
jwz-ecust <aaronzjw6@gmail.com>
2016-10-17 13:06:36 +0800

    -.-

---
+++ everyday/91tips/pingpong.py
@@ -0,0 +1,31 @@
+import itertools
+
+def check(a,b):
+    for i in zip(a,b):
+        if i in [('a','x'), ('c', 'x'), ('c', 'z')]:
+            return
+    else:
+        return a, b
+
+
+team_1 = ['a', 'b', 'c']
+team_2 = ['x', 'y', 'z']
+
+# for j in itertools.permutations(team_1, 3):
+#     print j
+#     print check(j, team_2)
+#     print "#"*50
+
+check_list = [('a', 'x'), ('c', 'x'), ('c', 'z')]
+# for i in itertools.permutations(team_1, 3):
+#     f = lambda a,b: len([True for j in zip(a, b) if j not in check_list])
+#     if f(i, team_2) == 3:
+#         print i, team_2
+
+
+for i in itertools.permutations(team_1, 3):
+    for j in zip(i, team_2):
+        if j in check_list:
+            break
+    else:
+        print i, team_2
