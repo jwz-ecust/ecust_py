5d22fd8c6b9eed1d5aa5fd8475b45e845e7f24e6
jwz-ecust <aaronzjw6@gmail.com>
2016-10-28 00:05:19 +0800

    -.-

--- Pybrain/examples/optimization/optimizers_for_reinforcement_learning.py
+++ Pybrain/examples/optimization/optimizers_for_reinforcement_learning.py
@@ -9,7 +9,7 @@ __author__ = 'Tom Schaul, tom@idsia.ch'
 from pybrain.utilities import fListToString
 from pybrain.rl.environments.cartpole.balancetask import BalanceTask
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.optimization import HillClimber, CMAES #@UnusedImport
+from pybrain.optimization import HillClimber, CMAES     # @UnusedImport
 # from pybrain.rl.learners.continuous.policygradients import ENAC
 # from pybrain.rl.agents.learning import LearningAgent
 from pybrain.rl.agents import OptimizationAgent
