16bc2f34d493cf39d505b146756d01ef2d7ca3e5
jwz-ecust <aaronzjw6@gmail.com>
2016-11-13 21:18:08 +0800

    a little

--- xsd/example/coulomb_matrix.py
+++ xsd/example/coulomb_matrix.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
 import os
 import numpy as np
 
-path = os.getcwd()
-path = os.path.join(path, 'example/1.xsd')
+'''
+库伦矩阵:
+Z_i: 代表i原子的核电荷数 (考虑价电子数???)
 
+C_ij = {0.5Z_i**2.4: i=j, Z_i*Z_j/(dis_between i and j): i!=j }
+
+NiP 001 surface    Ni: 28  P: 28
+'''
+
+
+def dis_cal(a, b):
+    '''
+    this is used to calculate the distence between two atoms
+    '''
+    return np.sqrt(sum((a - b)**2))
+
+
+path = '/Users/zhangjiawei/Code/zjw/xsd/example/NiP.xsd'
 root = ET.ElementTree(file=path)
 
-id_map = root.findall('.//IdentityMapping')
-print id_map
+charge_info = {'P': 5.0, 'Ni': 10.0}   # charge
 
 atom_list = []
 coordiante_list = []
+# get the atom coordiantes  and type
 for element in root.iter():
     # print "Tag:%s\nAttrib:%s\nText:%s" % (element.tag, element.attrib,
     #                                       element.text)
@@ -21,13 +37,16 @@ for element in root.iter():
         atom_list.append(atom)
         coordiante_list.append(coordiante)
 
+atom_array = np.array(coordiante_list, dtype=np.float)
+length = atom_array.shape[0]
+atom_matix = np.zeros((length, length))
+for i in range(length):
+    for j in range(length):
+        if i == j:
+            atom_matix[i][j] = 0.5 * charge_info[atom_list[i]]**2.4
+        else:
+            atom_matix[i][j] = charge_info[atom_list[
+                i]] * charge_info[atom_list[j]] / dis_cal(atom_array[i], atom_array[j])
 
-atom_matric = np.array(coordiante_list, dtype=np.float)
 
-t_atom = atom_matric[0]
-dis_list = []
-for i in range(1, len(atom_matric)):
-    dis = np.sqrt(sum((atom_matric[i] - t_atom)**2))
-    dis_list.append(dis)
-
-print dis_list
+print atom_matix
