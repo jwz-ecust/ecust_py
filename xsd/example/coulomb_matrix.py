839ec2075e50b2b65d3d4af21b58823101437bdb
jwz-ecust <aaronzjw6@gmail.com>
2016-11-13 21:50:37 +0800

    mdzz

--- xsd/example/coulomb_matrix.py
+++ xsd/example/coulomb_matrix.py
@@ -20,10 +20,23 @@ def dis_cal(a, b):
     return np.sqrt(sum((a - b)**2))
 
 
+def coulomb_effect_cal(i, j):
+    '''
+    this is used to calculate the coulomb effect
+    '''
+    charge_info = {'P': 5.0, 'Ni': 10.0}   # charge
+
+    if i == j:
+        effect = 0.5 * charge_info[atom_list[i]]**2.4
+    else:
+        effect = charge_info[atom_list[
+            i]] * charge_info[atom_list[j]] / dis_cal(atom_array[i], atom_array[j])
+    return effect
+
+
 path = '/Users/zhangjiawei/Code/zjw/xsd/example/NiP.xsd'
 root = ET.ElementTree(file=path)
 
-charge_info = {'P': 5.0, 'Ni': 10.0}   # charge
 
 atom_list = []
 coordiante_list = []
@@ -38,15 +51,24 @@ for element in root.iter():
         coordiante_list.append(coordiante)
 
 atom_array = np.array(coordiante_list, dtype=np.float)
+
+#  获取表面一个Ni site 的坐标
+zjw = atom_array[28:][:, 2]
+a = zjw.argmax()
+number = a + 28
+# 计算其他原子到Ni的距离, 库伦作用
+
+
+# 计算库伦矩阵
 length = atom_array.shape[0]
 atom_matix = np.zeros((length, length))
 for i in range(length):
     for j in range(length):
-        if i == j:
-            atom_matix[i][j] = 0.5 * charge_info[atom_list[i]]**2.4
-        else:
-            atom_matix[i][j] = charge_info[atom_list[
-                i]] * charge_info[atom_list[j]] / dis_cal(atom_array[i], atom_array[j])
+        atom_matix[i][j] = coulomb_effect_cal(i, j)
 
+# SVD 奇异值分解一下
+U, Sigma, VT = np.linalg.svd(atom_matix)
 
-print atom_matix
+# print U
+# print Sigma
+# print VT
