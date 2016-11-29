13d0226e722553a6d33f58a9632fc023886f8056
jwz-ecust <aaronzjw6@gmail.com>
2016-11-29 13:59:09 +0800

    qf ===> output:   id.server  id_full_path

--- xsd/catalysis/work_flow/qstatf.py
+++ xsd/catalysis/work_flow/qstatf.py
@@ -10,6 +10,7 @@ qstat_info = subprocess.check_output("qstat -f", shell=True)
 qstat_ = qstat_info.split("Job Id: ")
 for item in qstat_:
     item = item.split("    ")
+    id = item[0]
 
     for tag in item:
         if "job_state" in tag:
@@ -19,10 +20,10 @@ for item in qstat_:
 
         if "Output" in tag:
             out = tag.split('=')[-1].strip().split(':')[-1]
-            out = ''.join(out.split('\t'))
-            out = ''.join(out.split('\n'))
-            out = '/'.join(out.split('/')[:-1])
-            runlist.append(out)
+            pattern = re.compile(r'\n|\t')
+            out = re.sub(pattern, '', out)
+            out = os.path.dirname(out)
+            runlist.append((id.strip(), out))
 
 for i in runlist:
-    print i
+    print i[0], "    ", i[1]
